85df815dafb9b5a0b3fc18d07316c42634919406
ay4748 <ay4748@bs.amalnet.k12.il>
2023-12-15 21:03:56 +0200

    Final!

--- app/src/main/java/com/example/buttonex1/MainActivity.java
+++ app/src/main/java/com/example/buttonex1/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.buttonex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void dodge(View view) {
+        btn=findViewById(R.id.button);
+        btn.setText("Oh, yea, I’ve been clicked!");
     }
 }
